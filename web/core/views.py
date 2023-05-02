18101f3493aae6dc65c35bf6cf174e3b229f403f
madvirus-ops <edwinayabie1@gmail.com>
2023-05-02 13:59:59 +0100

    initializing the model

--- web/core/views.py
+++ web/core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import AnswerModel,QuestionModel
+from ..pipelines import pipeline
 
 
 # Create your views here.
@@ -7,6 +8,41 @@ from .models import AnswerModel,QuestionModel
 
 
 def index(request):
+    
+    if request.method == 'POST':
+        
+
+        #extract the required 
+        context = request.POST.post('context')
+        passage = request.POST.post('passage')
+    
+        #init the models
+        np = pipeline("multitask-qa-qg")
+
+        response = np(str(passage))
+        
+        print(response)
+
+
+        question = QuestionModel(
+            question=response['question']
+            
+        )
+        question.save()
+
+        answer = AnswerModel(
+            question=question,
+            answer=response['answer']
+        )
+
+        answer.save()
+
+        context = {"question":response['question'],
+                    "answer":response['answer']
+        
+        }
+        return render(request,'core/details.html',context)
+
     return render(request,'core/home.html')
 
 
