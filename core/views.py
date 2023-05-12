71eab65a331a0c13c463c9b29a8a600efa954194
madvirus-ops <edwinayabie1@gmail.com>
2023-05-12 11:58:41 +0100

    i did something

--- core/views.py
+++ core/views.py
@@ -60,18 +60,44 @@ def processPassage(request):
             return JsonResponse({"status":500,"error":str(e)})
 
 
-def processDocumentPassage(request):
-    file = request.FILES.get('pdf')
-    reder = PdfReader('file')
-    print(len(reder))
+def processDocumentPassage(passage_filename):
+    text = ""
+    paragraphs = []
+    with open(passage_filename, 'rb') as f:
+        reader = PdfReader(f)
+        for page in reader.pages:
+            text += page.extract_text()
 
-    page = reder.pages[0]
+    # yktv
+    paragraphs = text.split('\n\n')
 
-    text = page.extract_text()
-    print(text)
+    # na here i go split am into 400 words
+    sub_paragraphs = []
+    for paragraph in paragraphs:
+        words = paragraph.split()
+        sub_paragraph = ""
+        for word in words:
+            if len(sub_paragraph.split()) < 400:
+                sub_paragraph += word + " "
+            else:
+                sub_paragraphs.append(sub_paragraph.strip())
+                sub_paragraph = word + " "
+        if sub_paragraph.strip():
+            sub_paragraphs.append(sub_paragraph.strip())
+
+    questions = []
+    answers = []
+
+    for one_para in sub_paragraph:
+        response = np(str(one_para))
+                
+        for qa_pair in response:
+            questions.append(qa_pair['question'])            
+            answers.append(qa_pair['answer'])  
+        
+    return {"question":questions,"answer":answers}
+
+
     
-
-
-    """or"""
 
 
